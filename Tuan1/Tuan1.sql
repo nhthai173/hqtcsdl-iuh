9d72a13adebab9dd165a9e5ba1b6ebae924dcdb8
NTP <150322647+ntp03@users.noreply.github.com>
2025-01-10 12:48:08 +0700

    Upload files

---
+++ Tuan1/Tuan1.sql
@@ -0,0 +1,141 @@
+﻿CREATE DATABASE SmallWorks
+ON PRIMARY 
+(	NAME = 'SmallWorksPrimary',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\SmallWorks.mdf',
+	SIZE = 10MB,
+	FILEGROWTH = 20%,
+	MAXSIZE = 50MB	
+),
+FILEGROUP SWUserData1
+(	NAME = 'SmallWorksData1',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\SmallWorksData1.ndf',
+	SIZE = 10MB,
+	FILEGROWTH = 20%,
+	MAXSIZE = 50MB
+),
+FILEGROUP SWUserData2
+(	NAME = 'SmallWorksData2',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\SmallWorksData2.ndf',
+	SIZE = 10MB,
+	FILEGROWTH = 20%,
+	MAXSIZE = 50MB	
+)
+LOG ON
+(	NAME = 'SmallWorks_	log',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\SmallWorks_log.ldf',
+	SIZE = 10MB,
+	FILEGROWTH = 10%,
+	MAXSIZE = 20MB
+)
+
+--3. Dùng SSMS để xem kết quả: Click phải trên tên của CSDL vừa tạo
+--a. Chọn filegroups, quan sát kết quả:
+-- Có bao nhiêu filegroup, liệt kê tên các filegroup hiện tại
+-- Filegroup mặc định là gì?
+	--Có 3 Filegroup
+	--PRIMARY, SWUserData1, SWUserData2, SmallWorks_log.
+	--
+--b. Chọn file, quan sát có bao nhiêu database file?
+	-- 4 database file : SmallWorksPrimary,	SmallWorksData1, SmallWorksData2, 	
+
+
+
+--4. Dùng T-SQL tạo thêm một filegroup tên Test1FG1 trong SmallWorks, sau đó add
+--thêm 2 file filedat1.ndf và filedat2.ndf dung lượng 5MB vào filegroup Test1FG1.
+--Dùng SSMS xem kết quả.
+Use SmallWorks
+
+ALTER DATABASE SmallWorks
+ADD FILEGROUP Test1FG1
+
+ALTER DATABASE SmallWorks
+ADD FILE
+(
+	NAME = 'filedat1',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\filedat1.ndf',
+	SIZE = 5MB
+) TO FILEGROUP Test1FG1 
+
+ALTER DATABASE SmallWorks
+ADD FILE 
+(	NAME = 'filedat2',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\filedat2.ndf',
+	SIZE = 5MB
+) TO FILEGROUP Test1FG1 
+
+
+--5. Dùng T-SQL tạo thêm một một file thứ cấp filedat3.ndf dung lượng 3MB trong
+--filegroup Test1FG1. Sau đó sửa kích thước tập tin này lên 5MB. Dùng SSMS xem
+--kết quả. Dùng T-SQL xóa file thứ cấp filedat3.ndf. Dùng SSMS xem kết quả
+ALTER DATABASE SmallWorks
+ADD FILE
+(	NAME = 'filedat3',
+	FILENAME = 'D:\HOC\HK8\TH_CSDL\Tuan1\filedat3.ndf',
+	SIZE = 3MB
+) TO FILEGROUP Test1FG1
+
+ALTER DATABASE SmallWorks
+MODIFY FILE( NAME = 'filedat3',SIZE = 5MB)
+
+ALTER DATABASE SmallWorks
+REMOVE FILE filedat3
+
+--6. Xóa filegroup Test1FG1? Bạn có xóa được không? Nếu không giải thích? Muốn xóa
+--được bạn phải làm gì?
+ALTER DATABASE SmallWorks
+REMOVE FILEGROUP Test1FG1
+--Không xoá được vì trong filegroup vẫn còn dữ liệu của file
+--Muốn xoá thì phải xoá toàn bộ file bên trong file filegroup
+
+
+--7. Xem lại thuộc tính (properties) của CSDL SmallWorks bằng cửa sổ thuộc tính
+--properties và bằng thủ tục hệ thống sp_helpDb, sp_spaceUsed, sp_helpFile.
+--Quan sát và cho biết các trang thể hiện thông tin gì?.
+USE SmallWorks
+sp_helpDb --Truy vấn thông tin
+sp_spaceUsed --Kiểm tra kích thước và dung lượng
+sp_helpFIle --xem thông tin về filegroup của database hiện hành
+
+--8. Tại cửa sổ properties của CSDL SmallWorks, chọn thuộc tính ReadOnly, sau đó
+--đóng cửa sổ properties. Quan sát màu sắc của CSDL. Dùng lệnh T-SQL gỡ bỏ
+--thuộc tính ReadOnly và đặt thuộc tính cho phép nhiều người sử dụng CSDL
+--SmallWorks.
+
+
+ALTER DATABASE SmallWorks
+SET READ_WRITE
+
+ALTER DATABASE SmallWorks
+SET MULTI_USER
+
+--9. Trong CSDL SmallWorks, tạo 2 bảng mới theo cấu trúc như sau:
+
+CREATE TABLE Person
+(
+	PersonID int NOT NULL,
+	FirstName varchar(50) NOT NULL,
+	MiddleName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	EmailAddress nvarchar(50) NULL,
+)ON SWUserData1
+
+CREATE TABLE Product
+(
+	 ProductID int NOT NULL,
+	 ProductName varchar(75) NOT NULL,
+	 ProductNumber nvarchar(25) NOT NULL,
+	 StandardCost money NOT NULL,
+	 ListPrice money NOT NULL
+ ) ON SWUserData2
+
+--10. Chèn dữ liệu vào 2 bảng trên, lấy dữ liệu từ bảng Person và bảng Product trong
+--AdventureWorks2008 (lưu ý: chỉ rõ tên cơ sở dữ liệu và lược đồ), dùng lệnh
+--Insert…Select... Dùng lệnh Select * để xem dữ liệu trong 2 bảng Person và bảng
+--Product trong SmallWorks.
+
+
+
+
+
+
+
---
+++ Tuan3/Tuan3.sql
@@ -0,0 +1,84 @@
+﻿--1) Tạo view dbo.vw_Products hiển thị danh sách các sản phẩm từ bảng
+--Production.Product và bảng Production.ProductCostHistory. Thông tin bao gồm
+--ProductID, Name, Color, Size, Style, StandardCost, EndDate, StartDate
+
+USE AdventureWorks2008R2
+GO
+
+CREATE VIEW vw_Products
+AS
+	SELECT p.ProductID, p.Name, p.Color, p.Size, p.Style, pch.StandardCost, pch.EndDate, pch.StartDate
+	FROM 
+		[Production].[Product] p
+	JOIN
+		[Production].[ProductCostHistory] pch
+	ON
+		p.ProductID = pch.ProductID
+GO
+
+
+--2) Tạo view List_Product_View chứa danh sách các sản phẩm có trên 500 đơn đặt
+--hàng trong quí 1 năm 2008 và có tổng trị giá >10000, thôn	g tin gồm ProductID,
+--Product_Name, CountOfOrderID và SubTotal.
+
+-- CREATE VIEW List_Product_View
+-- AS	
+
+
+SELECT 
+	p.ProductID,
+	p.Name AS 'Product_Name',
+	COUNT(pod.PurchaseOrderID) AS 'CountOfOrderID',
+	SUM(pod.LineTotal) AS 'SubTotal'
+FROM
+	[Production].[Product] p
+	JOIN [Purchasing].[PurchaseOrderDetail] pod
+		ON p.ProductID = pod.ProductID
+	JOIN [Purchasing].[PurchaseOrderHeader] poh
+		ON pod.PurchaseOrderID = poh.PurchaseOrderID
+WHERE
+	YEAR(poh.OrderDate) = 2008
+	AND MONTH(poh.OrderDate) IN (1,2,3)
+GROUP BY
+	p.ProductID, p.Name
+HAVING
+	SUM(pod.LineTotal) > 10000
+	--AND COUNT(pod.PurchaseOrderID) > 500
+
+
+
+--3) Tạo view dbo.vw_CustomerTotals hiển thị tổng tiền bán được (total sales) từ cột
+--TotalDue của mỗi khách hàng (customer) theo tháng và theo năm. Thông tin gồm
+--CustomerID, YEAR(OrderDate) AS OrderYear, MONTH(OrderDate) AS
+--OrderMonth, SUM(TotalDue).
+--4) Tạo view trả về tổng số lượng sản phẩm (Total Quantity) bán được của mỗi nhân
+--viên theo từng năm. Thông tin gồm SalesPersonID, OrderYear, sumOfOrderQty
+--5) Tạo view ListCustomer_view chứa danh sách các khách hàng có trên 25 hóa đơn
+--đặt hàng từ năm 2007 đến 2008, thông tin gồm mã khách (PersonID) , họ tên
+--(FirstName +' '+ LastName as FullName), Số hóa đơn (CountOfOrders).
+--6) Tạo view ListProduct_view chứa danh sách những sản phẩm có tên bắt đầu với
+--‘Bike’ và ‘Sport’ có tổng số lượng bán trong mỗi năm trên 50 sản phẩm, thông tin
+--gồm ProductID, Name, SumOfOrderQty, Year. (dữ liệu lấy từ các bảng
+--Sales.SalesOrderHeader, Sales.SalesOrderDetail, và
+--Production.Product)
+--7) Tạo view List_department_View chứa danh sách các phòng ban có lương (Rate:
+--lương theo giờ) trung bình >30, thông tin gồm Mã phòng ban (DepartmentID),
+--tên phòng ban (Name), Lương trung bình (AvgOfRate). Dữ liệu từ các bảng
+--[HumanResources].[Department],[HumanResources].[EmployeeDepartmentHistory],
+--[HumanResources].[EmployeePayHistory].
+--8) Tạo view Sales.vw_OrderSummary với từ khóa WITH ENCRYPTION gồm
+--OrderYear (năm của ngày lập), OrderMonth (tháng của ngày lập), OrderTotal
+--(tổng tiền). Sau đó xem thông tin và trợ giúp về mã lệnh của view này
+--9) Tạo view Production.vwProducts với từ khóa WITH SCHEMABINDING
+--gồm ProductID, Name, StartDate,EndDate,ListPrice của bảng Product và bảng
+--ProductCostHistory. Xem thông tin của View. Xóa cột ListPrice của bảng
+--Product. Có xóa được không? Vì sao?
+--10) Tạo view view_Department với từ khóa WITH CHECK OPTION chỉ chứa các
+--phòng thuộc nhóm có tên (GroupName) là “Manufacturing” và “Quality
+--Assurance”, thông tin gồm: DepartmentID, Name, GroupName.
+--a. Chèn thêm một phòng ban mới thuộc nhóm không thuộc hai nhóm
+--“Manufacturing” và “Quality Assurance” thông qua view vừa tạo. Có
+--chèn được không? Giải thích.
+--b. Chèn thêm một phòng mới thuộc nhóm “Manufacturing” và một
+--phòng thuộc nhóm “Quality Assurance”.
+--c. Dùng câu lệnh Select xem kết quả trong bảng Department.
